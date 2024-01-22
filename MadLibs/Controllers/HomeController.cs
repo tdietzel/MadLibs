5cc252df13958b2f355470b18b2952b980849733
tdietzel22 <147360049+tdietzel22@users.noreply.github.com>
2024-01-22 11:36:27 -0800

    Add Christmas Ad Lib

--- MadLibs/Controllers/HomeController.cs
+++ MadLibs/Controllers/HomeController.cs
@@ -12,8 +12,9 @@ namespace MadLib.Controllers
     public ActionResult RoadTripForm() { return View(); }
     
     [Route("/road-trip")]
-    public ActionResult RoadTripLib(string emotionOne, string adjectiveOne, string placeOne, string thingOne, string thingTwo, string placeTwo, string foodOne, string thingThree, string thingFour, string treatOne, string placeThree, string adjectiveTwo) {
-      MadLibVariables newMadLib = new MadLibVariables();
+    public ActionResult RoadTripLib(string emotionOne, string adjectiveOne, string placeOne, string thingOne, string thingTwo, string placeTwo, string foodOne, string thingThree, string thingFour, string treatOne, string placeThree, string adjectiveTwo)
+    {
+      MadLibVariablesRoadTrip newMadLib = new MadLibVariablesRoadTrip();
       newMadLib.EmotionOne = emotionOne;
       newMadLib.AdjectiveOne = adjectiveOne;
       newMadLib.PlaceOne = placeOne;
@@ -26,6 +27,31 @@ namespace MadLib.Controllers
       newMadLib.TreatOne = treatOne;
       newMadLib.PlaceThree = placeThree;
       newMadLib.AdjectiveTwo = adjectiveTwo;
+      return View(newMadLib);
+    }
+
+    [Route("/christmas-form")]
+    public ActionResult ChristmasForm() { return View(); }
+
+    [Route("/christmas")]
+    public ActionResult ChristmasLib(string nounOne, string placeOne, string nounTwo, string verbOne, string animalOne, string adverbOne, string adjectiveOne, string onomatopoedicOne, string foodOne, string foodTwo, string adjectiveTwo, string clothingOne, string adjectiveThree, string exclamationOne, string numberOne)
+    {
+      MadLibVariablesChristmas newMadLib = new MadLibVariablesChristmas();
+      newMadLib.NounOne = nounOne;
+      newMadLib.PlaceOne = placeOne;
+      newMadLib.NounTwo = nounTwo;
+      newMadLib.VerbOne = verbOne;
+      newMadLib.AnimalOne = animalOne;
+      newMadLib.AdverbOne = adverbOne;
+      newMadLib.AdjectiveOne = adjectiveOne;
+      newMadLib.OnomatopoedicOne = onomatopoedicOne;
+      newMadLib.FoodOne = foodOne;
+      newMadLib.FoodTwo = foodTwo;
+      newMadLib.AdjectiveTwo = adjectiveTwo;
+      newMadLib.ClothingOne = clothingOne;
+      newMadLib.AdjectiveThree = adjectiveThree;
+      newMadLib.ExclamationOne = exclamationOne;
+      newMadLib.NumberOne = numberOne;
       return View(newMadLib);
     }
   }
--- MadLibs/Models/MadLib.cs
+++ MadLibs/Models/MadLib.cs
@@ -1,6 +1,6 @@
 namespace MadLib.Models
 {
-  public class MadLibVariables
+  public class MadLibVariablesRoadTrip
   {
     public string EmotionOne { get; set; }
     public string AdjectiveOne { get; set; }
@@ -14,5 +14,25 @@ namespace MadLib.Models
     public string TreatOne { get; set; }
     public string PlaceThree { get; set; }
     public string AdjectiveTwo { get; set; }
+
+  }
+
+  public class MadLibVariablesChristmas
+  {
+    public string NounOne { get; set; }
+    public string PlaceOne { get; set; }
+    public string NounTwo { get; set; }
+    public string VerbOne { get; set; }
+    public string AnimalOne { get; set; }
+    public string AdverbOne { get; set; }
+    public string AdjectiveOne { get; set; }
+    public string OnomatopoedicOne { get; set; }
+    public string FoodOne { get; set; }
+    public string FoodTwo { get; set; }
+    public string AdjectiveTwo { get; set; }
+    public string ClothingOne { get; set; }
+    public string AdjectiveThree { get; set; }
+    public string ExclamationOne { get; set; }
+    public string NumberOne {get; set; }
   }
 }
